012aa0237369f3900dc11d3506f31cf51dc6c8c9
WyattMallon <wyattmallon@gmail.com>
2025-01-21 15:07:02 -0500

    Implemented buildLong

--- Tools.C
+++ Tools.C
@@ -43,7 +43,11 @@
 */
 uint64_t Tools::buildLong(uint8_t bytes[LONGSIZE])
 {
-  return 0;
+	uint64_t output = 0;
+	for (uint8_t i = LONGSIZE; i > 0; i--) {
+		output = (output << 8) + bytes[i - 1];
+	}
+	return output;
 }
 
 /** 
